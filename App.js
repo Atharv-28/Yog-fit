261c3e496c8f922b5f1fced0d07ee4d769ad6870
Atharv-28 <atharvmt@outlook.com>
2024-01-17 14:28:13 +0530

    Bug fixed no more build break

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { Text, View, SafeAreaView, StyleSheet } from "react-native";
 import Profile from "./src/pages/profile";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./src/pages/home";
-const Stack = createStackNavigator();
 
 const App = () => {
   return (
