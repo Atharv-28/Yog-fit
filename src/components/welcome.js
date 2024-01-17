b12d726717708e349c661292eee751580506c3a4
Atharv-28 <atharvmt@outlook.com>
2024-01-17 15:03:07 +0530

    Optimizing Home Page 1

---
+++ src/components/welcome.js
@@ -0,0 +1,17 @@
+import { Text, View, StyleSheet } from "react-native";
+import React from "react";
+
+const Welcome = () => {
+  return;
+  <>
+    <Text>Welcome Back Shravan !!</Text>
+    <Image
+      source={{
+        uri: "https://th.bing.com/th/id/OIP.ag77IUhQeW_A-FQGcASMLAHaFj?w=220&h=180&c=7&r=0&o=5&pid=1.7",
+      }}
+      style={styles.image}
+    />
+  </>;
+};
+
+export default Welcome;
