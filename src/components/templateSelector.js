cf8ffc4362e8fca1962c056d83a114c5f0b39e1f
Atharv-28 <atharvmt@outlook.com>
2024-01-21 19:17:03 +0530

    template init

---
+++ src/components/templateSelector.js
@@ -0,0 +1,115 @@
+// Rank.js
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Platform,
+  StatusBar,
+} from "react-native";
+
+const allTemplates = [];
+const myTemplate = [];
+
+const TemplateSelector = () => {
+  const [activeTab, setActiveTab] = useState("myTemplate");
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <View style={styles.tabButtons}>
+          <TouchableOpacity onPress={() => setActiveTab("myTemplate")}>
+            <Text
+              style={[
+                styles.tabButton,
+                activeTab === "myTemplate" && styles.activeTab,
+              ]}
+            >
+              Global
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setActiveTab("globalTemplates")}>
+            <Text
+              style={[
+                styles.tabButton,
+                activeTab === "globalTemplate" && styles.activeTab,
+              ]}
+            >
+              Friends
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.tabContent}>
+          {activeTab === "globalTemplates" ? (
+            <Podium rankingData={allTemplates} />
+          ) : (
+            <Podium rankingData={myTemplate} />
+          )}
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 10,
+  },
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  tabButtons: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "#eee",
+    padding: 10,
+    borderColor: "red",
+    borderWidth: 2,
+    borderRadius: 30,
+  },
+  tabButton: {
+    fontSize: 16,
+  },
+  activeTab: {
+    fontWeight: "bold",
+    color: "blue",
+  },
+  tabContent: {
+    flex: 1,
+    padding: 10,
+    borderColor: "blue",
+    borderWidth: 2,
+  },
+  rankingContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    flexWrap: "wrap",
+    borderRadius: 30,
+    borderColor: "blue",
+    borderWidth: 2,
+  },
+  podiumItem: {
+    alignItems: "center",
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#ccc",
+  },
+  podiumPosition: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  topThree: {
+    flex: 2, // Make the top three items taller
+  },
+});
+
+export default TemplateSelector;
---
+++ src/pages/templatesPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  Platform,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
+import TemplateSelector from "../components/templateSelector";
+
+const TemplatePage = () => {
+  return (
+    <SafeAreaView>
+      <TemplateSelector />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
